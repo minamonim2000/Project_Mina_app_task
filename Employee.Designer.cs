bfa80a1d9b0a65800721cbd26640a50da9da6c24
Mina <Mina@DESKTOP-OKJR5TV>
2023-01-26 17:26:46 +0200

    add_image

--- Employee.Designer.cs
+++ Employee.Designer.cs
@@ -65,5 +65,7 @@
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label cc;
+        private System.Windows.Forms.PictureBox pictureBox5;
     }
 }
