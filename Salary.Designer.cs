3f46de5ae6caa3da41edae80d1c6755f9e5f7b8e
Mina <Mina@DESKTOP-OKJR5TV>
2023-01-26 18:18:36 +0200

    create  AddBtn

--- Salary.Designer.cs
+++ Salary.Designer.cs
@@ -58,7 +58,7 @@
         private System.Windows.Forms.TextBox DAYS;
         private System.Windows.Forms.Label label4
         private System.Windows.Forms.Button EditBtn;
-
+        private System.Windows.Forms.Button AddBtn;
 
 
     }
