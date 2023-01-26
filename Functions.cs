935e41741b0cbed69a784a8ea1e5cb42608d16ea
Mina <Mina@DESKTOP-OKJR5TV>
2023-01-26 17:19:16 +0200

    sec_task

--- Functions.cs
+++ Functions.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Project_Mina_app
 {
-    internal class Functions
+    public class Functions
     {
+        private SqlConnection con;
+        private SqlCommand cmd;
+        private DataTable dt;
+        private SqlDataAdapter sda;
+        private string Constr;
+        public Functions()
+        {
+            Constr = @"Data Source=.;Initial Catalog=Emp1;Integrated Security=True;Pooling=False";
+            con = new SqlConnection(Constr);
+            cmd = new SqlCommand();
+            cmd.Connection = con;
+        }
     }
 }
