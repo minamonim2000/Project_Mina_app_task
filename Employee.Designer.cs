9e5f43191c2266141c15415d0c57a347ce085beb
Mina <Mina@DESKTOP-OKJR5TV>
2023-01-26 17:23:28 +0200

    create_picture

--- Employee.Designer.cs
+++ Employee.Designer.cs
@@ -44,5 +44,7 @@
         #endregion
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
     }
 }
