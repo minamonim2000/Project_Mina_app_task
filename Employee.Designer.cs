1dab374478908371e2258eb434d9ca962288f68b
Mina <Mina@DESKTOP-OKJR5TV>
2023-01-26 17:23:51 +0200

    create lable

--- Employee.Designer.cs
+++ Employee.Designer.cs
@@ -46,5 +46,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox EmpName;
+        private System.Windows.Forms.Label label3;
     }
 }
