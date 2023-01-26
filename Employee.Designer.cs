4bc2254fc9f19950b74af22f0884301dda3a1d22
Mina <Mina@DESKTOP-OKJR5TV>
2023-01-26 17:27:37 +0200

    create_delete_btn

--- Employee.Designer.cs
+++ Employee.Designer.cs
@@ -70,5 +70,9 @@
         private System.Windows.Forms.PictureBox pictureBox4;
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button DeleteBtn;
+        private System.Windows.Forms.Button DeleteBtn;
+
     }
+
 }
