2575f1b82521402e879dc9a01bc8a4b7194383be
Mina <Mina@DESKTOP-OKJR5TV>
2023-01-26 18:15:33 +0200

    create label8

--- Salary.Designer.cs
+++ Salary.Designer.cs
@@ -53,6 +53,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label8;
 
 
     }
