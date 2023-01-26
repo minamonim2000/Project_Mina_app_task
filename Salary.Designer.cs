8a6daab22ca65e75a0ff00deabc677a96f159c20
Mina <Mina@DESKTOP-OKJR5TV>
2023-01-26 18:16:43 +0200

    create DAYS

--- Salary.Designer.cs
+++ Salary.Designer.cs
@@ -55,6 +55,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox DAYS;
+
+
 
     }
 }
